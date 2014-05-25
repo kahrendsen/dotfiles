b77d3bffb9f86b24ccd16795c5271837a9b6193d
Kendall Ahrendsen <kendall.ahrendsen@gmail.com>
2014-05-24 23:39:53 -0700

    i think it's done?

--- .bashrc
+++ .bashrc
@@ -120,10 +120,14 @@ export HISTSIZE=100000
 . /usr/share/autojump/autojump.sh
 
 #attempt to load git completion scripts
-if [[ -f "~/.git-prompt.sh" ]]; then else wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh fi
-if [[ -f "~/.git-completion.bash" ]]; then else wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash fi
+if [[ -f "~/.git-prompt.sh" ]]; then else wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > /dev/null fi
+if [[ -f "~/.git-completion.bash" ]]; then else wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > /dev/null fi
 source ~/.git-prompt.sh
 source ~/.git-completion.bash
+
+#This is executed every time we're about to show a prompt, so the sanest way to build PS1 is to use this
+#Otherwise we get totally unreadable strings from hell
+PROMPT_COMMAND="set_prompt"
 
 
 function set_prompt{
@@ -194,3 +198,121 @@ function up {
     	done
     fi
 }
+
+alias rm=gvfs-trash
+alias grep='grep --color=auto'
+alias ll='ls -Alv --group-directories-first'
+alias ls='ls -h --color=auto' #Always colorize and use sensible sizes
+alias la='ls -A'           #  Show hidden files
+alias lx='ls -lXB'         #  Sort by extension.
+alias lk='ls -lSr'         #  Sort by size, biggest last.
+alias lt='ls -ltr'         #  Sort by date, most recent last.
+alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
+alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
+alias lm='ll |most'        #  Pipe through 'most'
+alias lr='ll -R'           #  Recursive ls.
+alias tree='tree -Csh'    #  Nice alternative to 'recursive ls' ...
+alias mkdir='mkdir -p'
+alias more='most'
+alias less='most'
+# Pretty-print of some PATH variables:
+alias path='echo -e ${PATH//:/\\n}'
+alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
+
+function soffice() { command soffice "$@" & }
+function firefox() { command firefox "$@" & }
+function xpdf() { command xpdf "$@" & }
+
+
+function swap()
+{ # Swap 2 filenames around, if they exist (from Uzi's bashrc).
+    local TMPFILE=tmp.$$
+
+    [ $# -ne 2 ] && echo "swap: 2 arguments needed" && return 1
+    [ ! -e $1 ] && echo "swap: $1 does not exist" && return 1
+    [ ! -e $2 ] && echo "swap: $2 does not exist" && return 1
+
+    mv "$1" $TMPFILE
+    mv "$2" "$1"
+    mv $TMPFILE "$2"
+}
+
+function extract()      # Handy Extract Program
+{
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1     ;;
+            *.tar.gz)    tar xvzf $1     ;;
+            *.bz2)       bunzip2 $1      ;;
+            *.rar)       unrar x $1      ;;
+            *.gz)        gunzip $1       ;;
+            *.tar)       tar xvf $1      ;;
+            *.tbz2)      tar xvjf $1     ;;
+            *.tgz)       tar xvzf $1     ;;
+            *.zip)       unzip $1        ;;
+            *.Z)         uncompress $1   ;;
+            *.7z)        7z x $1         ;;
+            *)           echo "'$1' cannot be extracted via >extract<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
+
+
+# Creates an archive (*.tar.gz) from given directory.
+function maketar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
+
+# Create a ZIP archive of a file or folder.
+function makezip() { zip -r "${1%%/}.zip" "$1" ; }
+
+# Make your directories and files access rights sane.
+function sanitize() { chmod -R u=rwX,g=rX,o= "$@" ;}
+
+
+function killps()   # kill by process name
+{
+    local pid pname sig="-TERM"   # default signal
+    if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+        echo "Usage: killps [-SIGNAL] pattern"
+        return;
+    fi
+    if [ $# = 2 ]; then sig=$1 ; fi
+    for pid in $(my_ps| awk '!/awk/ && $0~pat { print $1 }' pat=${!#} )
+    do
+        pname=$(my_ps | awk '$1~var { print $5 }' var=$pid )
+        if ask "Kill process $pid <$pname> with signal $sig?"
+            then kill $sig $pid
+        fi
+    done
+}
+
+
+function getIP() # Get IP adress on ethernet.
+{
+    MY_IP=$(/sbin/ifconfig eth0 | awk '/inet/ { print $2 } ' |
+      sed -e s/addr://)
+    echo ${MY_IP:-"Not connected"}
+}
+
+
+function repeat()       # Repeat n times command.
+{
+    local i max
+    max=$1; shift;
+    for ((i=1; i <= max ; i++)); do  # --> C-like syntax
+        eval "$@";
+    done
+}
+
+
+
+
+
+#Stuff to print out at the beginning of the session
+echo "$(date \"%A %B %d %Y @ %r\")"
+echo "$(id -un)@$(hostname)"
+
+
+
+#Maybe just shoulda used this... https://github.com/nojhan/liquidprompt
--- setup.bash
+++ setup.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#A setup file for making the environment the way I like it. I kinda-sorta made it cross-platform-ish.
+#I hope for it to work on all flavors of Debian, Fedora, and OSX/Darwin. Maybe I will add arch and Cygwin later. I think openSUSE is RPM based so maybe those are covered too
+#Terminal emulator is an issue as well, this should hopefully support Gnome shell, Konsole, Xfce terminal, xterm, and iTerm2. Maybe I'll add Cygwin one day
+#Will probably never add Gentoo or Slackware
+
+
 # make the hard links so we don't have to move them back and forth to commit/push/pull
 ln .vimrc ~/
 ln .bashrc ~/
@@ -10,6 +16,9 @@ source ~/.bashrc
 #Kill CapsLock
 (dumpkeys | grep keymaps; echo "keycode 58 = Escape") | loadkeys
 
+#make sure bashrc loads in login shells too
+echo "source ~/.bashrc" >>
+
 
 #Don't do any fancy network stuff if simple is active
 if [ "$1" = "s" ];
@@ -17,37 +26,72 @@ then
 	exit 0
 fi
 
+#note how much space we have now so we know how much we used when we're through
+#hopefully this helps figure out if I can use this on, say, a Raspberry Pi
+usedSpaceStart=$(df --total | grep total | awk 'END{print $3;}')
+
+#major package managers: aptitude - debian, yum - fedora, homebrew - OSX, 
+installer="ERROR" #somehow didn't have aptitude or yum and we're not on OSX, or something failed
+#If we find yum, we also try to set two additional repos, because RHEL's defaults are kinda sucky. Not a big deal if this fails. Hopefully Fedora won't need this
+which yum && installer="sudo yum -y " && sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm && sudo rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+which aptitude && installer="sudo aptitude -y "
+uname | grep -i darwin && sudo ruby -e homebrew_install.rb && installer="homebrew " && brew update
+
+#make sure everything is up to date, unfortunately have to use upgrade cuz homebrew
+$installer upgrade
+
 #Try to get Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>&1 
 #git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>&1 || { echo >&2 "Git not installed, attempting to install..."; sudo apt-get -y install git-core; git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim }
 
+#make sure vim is installed
+$installer install vim || echo "Couldn't install Vim"
+
 #Install plugins
-vim +PluginInstall +qall
+vim +PluginInstall +qall || echo "Couldn't install vim plugins"
 
 #Install bro because I'm a noob
-sudo apt-get install rubygems
-gem install bropages
+$installer install rubygems || echo "Couldn't install rubygems"
+gem install bropages || echo "Couldn't install bro"
 
 #install most and set as default more, should color man pages and stuff
-apt-get install most && update-alternatives --set pager /usr/bin/most
+$installer install most && update-alternatives --set pager /usr/bin/most || echo "Could not install most"
+
+#make sure wget is installed
+$installer install wget
 
 #get git completion scripts
-wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh || echo "Couldn't fetch git-prompt.sh"
+wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash || echo "Couldn't fetch git-completion.bash"
+
+#install autojump
+$installer install autojump || echo "Couldn't install autojump"
+
+#install htop
+$installer install htop
+
+#install 
 
 #Go through all of the color schemes for popular terminal emulators and try to copy the correct files to the correct places
 #Those that don't match the current terminal emulator should simply fail. I may pipe the errors to dev/null later
 #iTerm2 needs to be set manually, the file to import is in the git repo
 
 #xfce-terminal
-cp terminalrc ~/.config/Terminal/terminalrc
+cp terminalrc ~/.config/Terminal/terminalrc || echo "Not Xfce"
 
 #gnome
-rake set scheme=solarized_dark
+rake set scheme=solarized_dark || echo "Not Gnome"
 
 #xterm
-cp .Xresources ~
+which xterm && cp .Xresources && xrdb -merge ~/.Xresources~ || echo "Not xterm"
 
 #Konsole
-cp SolarizedDark.colorscheme ~/.kde/share/apps/konsole
+cp SolarizedDark.colorscheme ~/.kde/share/apps/konsole || echo "Not Konsole"
 
+
+usedSpaceEnd=$(df --total | grep total | awk 'END{print $3;}')
+installSpace=$usedSpaceEnd-$usedSpaceStart
+kilo=$installSpace%1024
+mega=$installSpace/1024
+giga=$mega/1024
+echo "Installation used $giga GB, $mega MB, $kilo KB"
